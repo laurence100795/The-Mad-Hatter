f051e0d37aaa0a90b87a1fb8ca7af3dad9aff064
laurence100795 <laurence100795@gmail.com>
2020-09-10 11:50:13 +0000

    navbar structure complete

--- mad_hatter/settings.py
+++ mad_hatter/settings.py
@@ -145,8 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# pushes styles from static file across the app
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+# pushes images stored in media file across the app
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
