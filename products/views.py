075e8e2ce3ec8bba4a5b89a3ab4d75cb70918843
laurence100795 <laurence100795@gmail.com>
2020-10-23 17:50:27 +0000

    added selection box, product counter, product categories and changes to products css

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -18,7 +19,7 @@ def all_products(request):  # view to show all products
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=('name'))
+                products = products.annotate(lower_name=Lower('name'))
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
