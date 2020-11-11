0a562581e4a5449bb7c3bfa2ca160c5356a73d0e
laurence100795 <laurence100795@gmail.com>
2020-11-11 18:11:05 +0000

    implemented stripe

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H7NrpEfDB6nIOBgPpSlLJ64jAe7j853XqDQZuiIkv1keEnkCKGIsbGtm5eOiS3PDmA5dkpjPnwgd2OiY7zoqdLJ00YsG5iAFK',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
