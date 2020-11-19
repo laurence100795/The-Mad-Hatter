7722d37b628c0c8dab675f64263731ed18364835
laurence100795 <laurence100795@gmail.com>
2020-11-19 18:44:11 +0000

    updated checkout view/ added order display to profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
     # Display the user's profile.
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile information updated')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all
@@ -14,6 +22,21 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f'This is previous order {order_number}.')
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
