6125cc4953f6cb08929b7dd5a3ce4c2cabcdcf79
laurence100795 <laurence100795@gmail.com>
2020-12-03 01:34:55 +0000

    further webhook decomission

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import (render, redirect, reverse,
-                              get_object_or_404, HttpResponse)
-from django.views.decorators.http import require_POST
+                              get_object_or_404,)
 from django.contrib import messages
 from django.conf import settings
 from django.core.mail import send_mail
@@ -14,24 +13,6 @@ from profiles.forms import UserProfileForm
 from profiles.models import UserProfile
 
 import stripe
-import json
-
-
-@require_POST
-def cache_checkout_data(request):
-    try:
-        pid = request.POST.get('client_secret').split('_secret')[0]
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag', {})),
-            'save_info': request.POST.get('save_info'),
-            'username': request.user,
-        })
-        return HttpResponse(status=200)
-    except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
-        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -62,13 +43,12 @@ def checkout(request):
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
-                    if isinstance(item_data, int):
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                            quantity=item_data,
-                        )
-                        order_line_item.save()
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
                 except Product.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your bag \
